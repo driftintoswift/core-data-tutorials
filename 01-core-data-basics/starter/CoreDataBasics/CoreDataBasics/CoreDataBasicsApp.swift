697ab44985fa28dff215f7be97f9031ce3c2afae
Karel Busta <karelbusta@gmail.com>
2022-01-21 11:51:15 +0100

    initial commit with core-data-basics/starter

---
+++ 01-core-data-basics/starter/CoreDataBasics/CoreDataBasics/CoreDataBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBasicsApp.swift
+//  CoreDataBasics
+//
+//  Created by Karel Bušta on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
